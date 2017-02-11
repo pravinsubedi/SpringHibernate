c2774c617c25d82fc0f6b97baab042eafbef134f
pravinsubedi <mepravinsubedi@gmail.com>
2017-02-11 12:07:55 +0545

    adds many to one relationship

--- src/main/java/com/hibernate/entity/Student.java
+++ src/main/java/com/hibernate/entity/Student.java
@@ -1,10 +1,11 @@
 package com.hibernate.entity;
 
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
 import javax.annotation.Generated;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 /**
  * Created by pravin on 2/10/2017.
@@ -15,14 +16,10 @@ public class Student {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int student_id;
     private String student_name;
-
-    public Student(int student_id, String student_name) {
-        this.student_id = student_id;
-        this.student_name = student_name;
-    }
-
-    public Student() {
-    }
+    @OneToMany(cascade = CascadeType.ALL)
+    @LazyCollection(LazyCollectionOption.FALSE)
+    @JoinColumn(name = "student_id")
+    private List<StudentAddress> studentAddress;
 
     public int getStudent_id() {
         return student_id;
@@ -38,6 +35,26 @@ public class Student {
 
     public void setStudent_name(String student_name) {
         this.student_name = student_name;
+    }
+
+    public List<StudentAddress> getStudentAddress() {
+        return studentAddress;
+    }
+
+    public void setStudentAddress(List<StudentAddress> studentAddress) {
+        this.studentAddress = studentAddress;
+    }
+
+    public Student(String student_name, List<StudentAddress> studentAddress) {
+        this.student_name = student_name;
+        this.studentAddress = studentAddress;
+    }
+
+    public Student(List<StudentAddress> studentAddress) {
+        this.studentAddress = studentAddress;
+    }
+
+    public Student() {
     }
 }
 
--- src/main/java/com/hibernate/entity/StudentAddress.java
+++ src/main/java/com/hibernate/entity/StudentAddress.java
@@ -1,7 +1,42 @@
 package com.hibernate.entity;
 
+import javax.persistence.*;
+import java.util.List;
+
 /**
  * Created by pravin on 2/10/2017.
  */
+@Entity
 public class StudentAddress {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private  int add_id;
+    private  String address;
+
+
+    public StudentAddress(String address) {
+        this.address = address;
+    }
+
+    public int getStudent_id() {
+        return add_id;
+    }
+
+    public void setStudent_id(int student_id) {
+        this.add_id= student_id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public StudentAddress() {
+    }
+
+
 }
+
